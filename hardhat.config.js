a93e9924c818482ba3d69286e5bfb88fc3f27d9d
Fan03z <2793791408@qq.com>
2023-05-04 10:55:37 +0800

    Foundry Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("solidity-coverage");
 require("hardhat-gas-reporter");
 require("hardhat-contract-sizer");
 require("dotenv").config();
+require("@nomicfoundation/hardhat-foundry");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
